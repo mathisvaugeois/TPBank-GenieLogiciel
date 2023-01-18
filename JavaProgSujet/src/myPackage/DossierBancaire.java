42d13a52f7ed0c95fa8634b006be41af1b575c71
mathisvaugeois <122368804+mathisvaugeois@users.noreply.github.com>
2023-01-18 19:58:01 +0100

    Commentaire
    Ajout de commentaire sur Dossier Bancaire

--- JavaProgSujet/src/myPackage/DossierBancaire.java
+++ JavaProgSujet/src/myPackage/DossierBancaire.java
@@ -8,16 +8,16 @@ public class DossierBancaire
 	
     public DossierBancaire() //constructeur du dossier bancaire
     {
-    	m_solde=0;
+    	m_solde = 0;
     	_compteCourant = new Compte(); //Creation d'un compte
     	_compteEpargne = new Compte();
     }
 
-    public void deposer(double somme) //d�poser de l'argent sur les deux comptes courant et �pargne
+    public void deposer(double somme) //déposer de l'argent sur les deux comptes courant et épargne
     {
     	m_solde += somme;
-    	_compteCourant.deposer(0.4 * somme);
-    	_compteEpargne.deposer(0.6 * somme);
+    	_compteCourant.deposer(0.4 * somme); //Depose 40% de la somme
+    	_compteEpargne.deposer(0.6 * somme); //Depose 60% de la somme
     }
     
     public double get_solde() //renvoie le solde du dossier
@@ -35,16 +35,16 @@ public class DossierBancaire
     	return _compteEpargne;
     }
     
-    public void remunerer() //verser les int�r�ts sur le compte �pargne
+    public void remunerer() //verser les intérêts sur le compte épargne
     {
-    	double somme = _compteEpargne.getSolde();
-    	somme = 0.032 * somme;
-    	_compteEpargne.deposer(somme);
+    	double somme = _compteEpargne.getSolde(); //Recupere le solde actuelle
+    	somme = 0.032 * somme; //Fait le calcul
+    	_compteEpargne.deposer(somme); //Mise a jour le solde
     }
     
     public void retrait(double somme)
     {
-    	double current_value = _compteCourant.getSolde();
+    	double current_value = _compteCourant.getSolde(); //Recupere le solde
     	if(somme <= current_value)
     	{
     		//OK POUR LE RETRAIT
